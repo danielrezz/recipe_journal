7c2b13e365fcf0b8e9b3e658cbc5f2709523dead
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-28 07:48:44 -0500

    updating path for express.static()

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const { v4: uuidv4 } = require('uuid');
 const fs = require("fs");
-// const dbJSON = require("./db.json");
+const dbJSON = require("./db.json");
 const path = require("path");
 
 // Sets up the Express App
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 // Routes
 // =============================================================
