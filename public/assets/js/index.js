514b7f6da155a1cfa792f1d674b3f45b14e2f6df
Daniel Reza <daniel.reza1007@gmail.com>
2022-01-21 00:46:42 -0600

    updating recipes, recipe save, and style

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,112 +1,118 @@
-const $noteTitle = $(".note-title");
-const $noteText = $(".note-textarea");
-const $saveNoteBtn = $(".save-note");
-const $newNoteBtn = $(".new-note");
-const $noteList = $(".list-container .list-group");
-const $delBtn = $("<i class='fas fa-trash-alt float-right text-danger delete-note'>");
+const $recipeTitle = $(".recipe-title");
+const $recipeText1 = $(".recipe-ingredients");
+const $recipeText2 = $(".recipe-steps");
+const $saveRecipeBtn = $(".save-recipe");
+const $newRecipeBtn = $(".new-recipe");
+const $recipeList = $(".list-container .list-group");
+const $delBtn = $("<i class='fas fa-trash-alt float-right text-danger delete-recipe'>");
 
-// activeNote is used to keep track of the note in the textarea
-let activeNote = {};
+// activeRecipe is used to keep track of the recipe in the textarea
+let activeRecipe = {};
 
-// A function for getting all notes from the db
-const getNotes = () => {
+// A function for getting all recipes from the db
+const getRecipes = () => {
   return $.ajax({
-    url: '/api/notes',
+    url: '/api/recipes',
     method: "GET"
   });
 };
 
-// A function for saving a note to the db
-const saveNote = (note) => {
+// A function for saving a recipe to the db
+const saveRecipe = (recipe) => {
   return $.ajax({
-    url: '/notes',
-    data: note,
+    url: '/recipes',
+    data: recipe,
     method: "POST"
   });
 };
 
-// A function for deleting a note from the db
-const deleteNote = (id) => {
+// A function for deleting a recipe from the db
+const deleteRecipe = (id) => {
   return $.ajax({
-    url: '/api/notes/' + id,
+    url: '/api/recipes/' + id,
     method: "DELETE"
   });
 };
 
-// If there is an activeNote, display it, otherwise render empty inputs
-const renderActiveNote = () => {
-  $saveNoteBtn.hide();
+// If there is an activeRecipe, display it, otherwise render empty inputs
+const renderActiveRecipe = () => {
+  $saveRecipeBtn.hide();
 
-  if (activeNote.id) {
-    $noteTitle.attr("readonly", true);
-    $noteText.attr("readonly", true);
-    $noteTitle.val(activeNote.title);
-    $noteText.val(activeNote.text);
+  if (activeRecipe.id) {
+    $recipeTitle.attr("readonly", true);
+    $recipeText1.attr("readonly", true);
+    $recipeText2.attr("readonly", true);
+    $recipeTitle.val(activeRecipe.title);
+    $recipeText1.val(activeRecipe.text1);
+    $recipeText2.val(activeRecipe.text2);
   } else {
-    $noteTitle.attr("readonly", false);
-    $noteText.attr("readonly", false);
-    $noteTitle.val("");
-    $noteText.val("");
+    $recipeTitle.attr("readonly", false);
+    $recipeText1.attr("readonly", false);
+    $recipeText2.attr("readonly", false);
+    $recipeTitle.val("");
+    $recipeText1.val("");
+    $recipeText2.val("");
   };
 };
 
-// Get the note data from the inputs, save it to the db and update the view
-const handleNoteSave = function () {
-  const newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val(),
+// Get the recipe data from the inputs, save it to the db and update the view
+const handleRecipeSave = function () {
+  const newRecipe = {
+    title: $recipeTitle.val(),
+    text1: $recipeText1.val(),
+    text2: $recipeText2.val(),
   };
 
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
+  saveRecipe(newRecipe).then(() => {
+    getAndRenderRecipes();
+    renderActiveRecipe();
   });
 };
 
-// Delete the clicked note
-const handleNoteDelete = function (event) {
+// Delete the clicked recipe
+const handleRecipeDelete = function (event) {
   // prevents the click listener for the list from being called when the button inside of it is clicked
   event.stopPropagation();
 
-  const note = $(this).parent(".list-group-item").data();
+  const recipe = $(this).parent(".list-group-item").data();
 
-  if (activeNote.id === note.id) {
-    activeNote = {};
+  if (activeRecipe.id === recipe.id) {
+    activeRecipe = {};
   }
 
-  deleteNote(note.id).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
+  deleteRecipe(recipe.id).then(() => {
+    getAndRenderRecipes();
+    renderActiveRecipe();
   });
 };
 
-// Sets the activeNote and displays it
-const handleNoteView = function () {
-  activeNote = $(this).data();
-  renderActiveNote();
+// Sets the activeRecipe and displays it
+const handleRecipeView = function () {
+  activeRecipe = $(this).data();
+  renderActiveRecipe();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
-const handleNewNoteView = function () {
-  activeNote = {};
-  renderActiveNote();
+// Sets the activeRecipe to and empty object and allows the user to enter a new recipe
+const handleNewRecipeView = function () {
+  activeRecipe = {};
+  renderActiveRecipe();
 };
 
-// If a note's title or text are empty, hide the save button
+// If a recipe's title or text are empty, hide the save button
 // Or else show it
 const handleRenderSaveBtn = function () {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
-    $saveNoteBtn.hide();
+  if (!$recipeTitle.val().trim() || !$recipeText1.val().trim() || !$recipeText2.val().trim()) {
+    $saveRecipeBtn.hide();
   } else {
-    $saveNoteBtn.show();
+    $saveRecipeBtn.show();
   };
 };
 
-// Render's the list of note titles
-const renderNoteList = (notes) => {
-  $noteList.empty();
+// Render's the list of recipe titles
+const renderRecipeList = (recipes) => {
+  $recipeList.empty();
 
-  const noteListItems = [];
+  const recipeListItems = [];
 
   // Returns jquery object for li with given text and delete button
   // unless withDeleteButton argument is provided as false
@@ -117,37 +123,38 @@ const renderNoteList = (notes) => {
 
     if (withDeleteButton) {
       const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        "<i class='fas fa-trash-alt float-right text-danger delete-recipe'>"
       );
       $li.append($delBtn);
     };
     return $li;
   };
 
-  if (notes.length === 0) {
-    noteListItems.push(create$li("No saved Notes", false));
+  if (recipes.length === 0) {
+    recipeListItems.push(create$li("No saved recipes", false));
   };
 
-  Array.from(notes).forEach((note) => {
-    const $li = create$li(note.title).data(note);
-    noteListItems.push($li);
+  Array.from(recipes).forEach((recipe) => {
+    const $li = create$li(recipe.title).data(recipe);
+    recipeListItems.push($li);
   });
 
-  $noteList.append(noteListItems);
+  $recipeList.append(recipeListItems);
 };
 
-// Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => {
-  return getNotes().then(renderNoteList);
+// Gets recipes from the db and renders them to the sidebar
+const getAndRenderRecipes = () => {
+  return getRecipes().then(renderRecipeList);
 };
 
-// $delBtn.on("click", handleNoteDelete);
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".list-group-item", handleNoteView);
-$newNoteBtn.on("click", handleNewNoteView);
-$noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
+// $delBtn.on("click", handleRecipeDelete);
+$saveRecipeBtn.on("click", handleRecipeSave);
+$recipeList.on("click", ".list-group-item", handleRecipeView);
+$newRecipeBtn.on("click", handleNewRecipeView);
+$recipeList.on("click", ".delete-recipe", handleRecipeDelete);
+$recipeTitle.on("keyup", handleRenderSaveBtn);
+$recipeText1.on("keyup", handleRenderSaveBtn);
+$recipeText2.on("keyup", handleRenderSaveBtn);
 
-// Gets and renders the initial list of notes
-getAndRenderNotes();
+// Gets and renders the initial list of recipes
+getAndRenderRecipes();
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 // Dependencies
 // =============================================================
-const express = require("express");
+const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-const fs = require("fs");
-let dbJSON = require("./db.json");
-const path = require("path");
-// const store = require('./store.js');
+const fs = require('fs');
+let dbJSON = require('./db.json');
+const path = require('path');
 
-// Sets up the Express App
-// =============================================================
 const app = express();
 let PORT = process.env.PORT || 3000;
 
@@ -17,28 +14,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Routes
-// =============================================================
-
-app.get('/notes', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/notes.html'));
+app.get('/recipes', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/recipes.html'));
 });
 
-app.delete("/api/notes/:id", function (req, res) {
+app.delete("/api/recipes/:id", function (req, res) {
   fs.readFile("db.json", function (err, data) {
     if (err) throw err;
-    let allNotes = JSON.parse(data);
-    let newNotes = allNotes.filter((note) => {
-      if(note.id !== req.params.id) {
+    let allRecipes = JSON.parse(data);
+    let newRecipes = allRecipes.filter((recipe) => {
+      if(recipe.id !== req.params.id) {
         return true;
       }
     });
-    fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(newNotes), (err) => {
+    fs.writeFile(path.join(__dirname, "db.json"), 
+    JSON.stringify(newRecipes), (err) => {
       if (err) {
         return res.json({error: "Error writing to file"});
       }
   
-      return res.json(newNotes);
+      return res.json(newRecipes);
     });
   });
 });
@@ -47,54 +42,62 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html')); 
 });
 
-app.get("/api/notes", function (req, res) {
+app.get("/api/recipes", function (req, res) {
   fs.readFile("db.json", function (err, data) {
     if (err) throw err;
-    let allNotes = JSON.parse(data);
-    return res.json(allNotes);
+    let allRecipes = JSON.parse(data);
+    return res.json(allRecipes);
   });
 });
 
-app.post('/notes', function(req, res) {
+app.post('/recipes', function(req, res) {
   // Validate request body
   if(!req.body.title) {
     return res.json({error: "Missing required title"});
   }
 
   // Copy request body and generate ID
-  const note = {...req.body, id: uuidv4()}
+  const recipe = {...req.body, id: uuidv4()}
 
-  // Push note to dbJSON array - saves data in memory
-  dbJSON.push(note);
+  // Push recipe to dbJSON array - saves data in memory
+  dbJSON.push(recipe);
 
   // Saves data to file by persisting in memory variable dbJSON to db.json file.
   // This is needed because when we turn off server we loose all memory data like pbJSON variable.
-  // Saving to file allows us to read previous notes (before server was shutdown) from file.
-  fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON, null, 2), (err) => {
-    if (err) {
-      return res.json({error: "Error writing to file"});
-    }
+  // Saving to file allows us to read previous recipes (before server was shutdown) from file.
+  // fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(dbJSON, null, 2), (err) => {
+  //   if (err) {
+  //     return res.json({error: "Error writing to file"});
+  //   }
 
-    return res.json(note);
+  //   return res.json(recipe);
+  // });
+
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedRecipes = JSON.parse(data);
+
+      // Add a new review
+      parsedRecipes.push(recipe);
+
+      // Write updated recipes back to the file
+      fs.writeFile(
+        'db.json',
+        JSON.stringify(parsedRecipes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated recipes!')
+      );
+
+      return res.json(recipe);
+    }
   });
 });
 
-// app.get('/api/notes', function(req, res) {
-//   fs.readFile("./db.json", function(err, data) {
-//     if (err) throw err;
-//     let allNotes = JSON.parse(data);
-//     return res.json(allNotes);
-//   });
-//   console.log(allNotes);
-// });
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/Develop/public/index.html'));
-  // res.send("Sending you the homepage");
-// });
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
